e833a328e7b9a1ac0b7c95d9d5468aabac08877b
José Santos <sjse7780@gmail.com>
2022-11-20 22:51:35 +0000

    Final Transform Commit , StringTransformerGroup

---
+++ src/main/java/com/patterns/StringTransformerGroup.java
@@ -0,0 +1,33 @@
+package com.patterns;
+
+import com.patterns.transform.StringTransformer;
+
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer{
+    private List<StringTransformer> transformers;
+
+    public StringTransformerGroup(List<StringTransformer> transformers) {
+        this.transformers = transformers;
+    }
+
+    public void mix(StringDrink stringDrink) {
+        for (StringTransformer transformer : transformers) {
+            transformer.execute(stringDrink);
+        }
+    }
+
+    @Override
+    public void execute(StringDrink drink) {
+        for (StringTransformer transformer : transformers) {
+            transformer.execute(drink);
+        }
+    }
+
+    @Override
+    public void undo(StringDrink drink) {
+        for (StringTransformer transformer : transformers) {
+            transformer.undo(drink);
+        }
+    }
+}
--- src/test/java/StringDrinkTest.java
+++ src/test/java/StringDrinkTest.java
@@ -1,5 +1,6 @@
 import com.patterns.StringDrink;
 import com.patterns.StringRecipe;
+import com.patterns.StringTransformerGroup;
 import com.patterns.transform.StringCaseChanger;
 import com.patterns.transform.StringInverter;
 import com.patterns.transform.StringReplacer;
@@ -69,5 +70,41 @@ public class StringDrinkTest {
         assertEquals("DcBa-dCbA", drink.getText());
         si.undo(drink);
         assertEquals("AbCd-aBcD", drink.getText());
+    }
+    @Test
+    public void transformerGroup() {
+        StringDrink drink = new StringDrink( "AbCd-aBcD");
+        StringInverter si = new StringInverter();
+        StringCaseChanger cc = new StringCaseChanger();
+        List<StringTransformer> transformers = new ArrayList<>();
+        transformers.add(si);
+        transformers.add(cc);
+        StringTransformerGroup tg = new
+                StringTransformerGroup(transformers);
+        tg.execute(drink);
+        assertEquals("dCbA-DcBa", drink.getText());
+    }
+    @Test
+    public void transformerComposite() {
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringInverter si = new StringInverter();
+        StringCaseChanger cc = new StringCaseChanger();
+        StringReplacer sr = new StringReplacer('A', 'X');
+        List<StringTransformer> transformers1 = new ArrayList<>();
+        transformers1.add(si);
+        transformers1.add(cc);
+        StringTransformerGroup tg1 = new
+                StringTransformerGroup(transformers1);
+        List<StringTransformer> transformers2 = new ArrayList<>();
+        transformers2.add(sr);
+        transformers2.add(cc);
+        StringTransformerGroup tg2 = new
+                StringTransformerGroup(transformers2);
+        List<StringTransformer> transformers3 = new ArrayList<>();
+        transformers3.add(tg1);
+        transformers3.add(tg2);
+        StringRecipe recipe = new StringRecipe(transformers3);
+        recipe.mix(drink);
+        assertEquals("DcBx-dCbA", drink.getText());
     }
 }
